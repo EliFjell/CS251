55a1fce1b3469c483063f02d9d716c8403ec6d5f
EliFjell <elifjell13@gmail.com>
2026-04-29 19:49:48 +0200

    question 1

--- 2_Perform_Bitcoin_transactions_using_python-bitcoinlib/Q1.py
+++ 2_Perform_Bitcoin_transactions_using_python-bitcoinlib/Q1.py
@@ -8,10 +8,12 @@ from lib.config import (my_private_key, my_public_key, my_address,
 
 def P2PKH_scriptPubKey(address):
     ######################################################################
-    # TODO: Complete the standard scriptPubKey implementation for a
-    # PayToPublicKeyHash transaction
     return [
-        # fill this in!
+        OP_DUP,
+        OP_HASH160,
+        address,
+        OP_EQUALVERIFY,
+        OP_CHECKSIG
     ]
     ######################################################################
 
@@ -20,10 +22,9 @@ def P2PKH_scriptSig(txin, txout, txin_scriptPubKey, private_key, public_key):
     signature = create_OP_CHECKSIG_signature(txin, txout, txin_scriptPubKey,
                                              private_key)
     ######################################################################
-    # TODO: Complete this script to unlock the BTC that was sent to you
-    # in the PayToPublicKeyHash transaction.
     return [
-        # fill this in!
+        signature,
+        public_key
     ]
     ######################################################################
 
@@ -52,11 +53,10 @@ def send_from_P2PKH_transaction(amount_to_send,
 
 if __name__ == '__main__':
     ######################################################################
-    # TODO: set these parameters correctly
-    amount_to_send = None # amount of BTC in the output you're sending minus fee
+    amount_to_send = 0.00015 # amount of BTC in the output you're sending minus fee
     txid_to_spend = (
-        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-    utxo_index = None # index of the output you are spending, indices start at 0
+        '38aebab9a43b3ff274f2683af451c1bd877c226f2f33c1fe6a06150c4b8d0052')
+    utxo_index = 2 # index of the output you are spending, indices start at 0
     ######################################################################
 
     txout_scriptPubKey = P2PKH_scriptPubKey(faucet_address)
